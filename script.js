cf8a9f236bf0010499b80a2e0b643701d593899d
Mateusz Górski <mgorski.lbn@gmail.com>
2018-11-27 00:13:04 +0100

    Increment/decrement counter homework

---
+++ script.js
@@ -0,0 +1,41 @@
+const Counter = (function counterIIFE() {
+    let currentValue = 0;
+    return {
+        currentValue: function() {
+          return currentValue;
+        },
+        incrementCounter: function() {
+            currentValue += 1;
+            return currentValue;
+        },
+        decrementCounter: function(){
+            currentValue -= 1;
+            return currentValue;
+        }
+    }
+}());
+
+const counter = document.getElementById('counter');
+
+document.getElementById('counter').innerText = Counter.currentValue()
+
+document.getElementById('increment').addEventListener('click', function(){
+    counter.innerText = Counter.incrementCounter()
+    createToast('WOOW! Udało się!!');
+})
+
+document.getElementById('decrement').addEventListener('click', function(){
+    counter.innerText = Counter.decrementCounter()
+    createToast('To działa!NIESAMOWITE');
+})
+
+function createToast(message) {
+    const toast = document.createElement('div');
+    toast.id = 'toast';
+    toast.innerText = message;
+    document.body.appendChild(toast);
+    setTimeout(() => {
+        toast.remove()
+    }, 1500)
+}
+
